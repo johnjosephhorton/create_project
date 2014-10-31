f8dda29e7a2836abae00b671501ba6ec5700d7b8
John Horton <john.joseph.horton@gmail.com>
2014-10-31 08:15:25 -0400

    Update school name

--- settings.py
+++ settings.py
@@ -1,20 +1,9 @@
+
 author = "John J. Horton" 
-school = "NYU Stern" 
+
+school = "Leonard N. Stern School of Business\\ New York University" 
+
 footnote = """Author contact information, datasets and code are currently or will be available at \\href{http://www.john-joseph-horton.com/}{http://www.john-joseph-horton.com/}."""
+
 bibliography_style = "aer" 
 
-dirs = [
-    'literature', 
-    'code/R', 
-    'code/R/tests',
-    'code/SQL', 
-    'code/python', 
-    'data',
-    'models',    
-    'submit', 
-    'writeup/images', 
-    'writeup/numbers', 
-    'writeup/plots', 
-    'writeup/tables',
-    'writeup/diagrams', 
-]
